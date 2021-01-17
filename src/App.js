8caecf2fcbfe4c04cd430d14498d4c0f2dd34545
minh1590711 <minh1590711@gmail.com>
2021-01-17 10:41:29 +0700

    Publist app to repo

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import './App.css';
+import React from 'react';
+import 'antd/dist/antd.css';
+import { Layout } from 'antd';
+import AppHeader from './components/common/header';
+import AppHome from './views/home';
+import AppFooter from './components/common/footer';
+
+const { Header, Content, Footer } = Layout;
+
+
+function App() {
+  return (
+    <Layout className="mainLayout">
+    <Header>
+      <AppHeader/>
+    </Header>
+    <Content>
+      <AppHome/>
+    </Content>
+    <Footer>
+      <AppFooter/>
+    </Footer>
+  </Layout>
+  );
+}
+
+export default App;
